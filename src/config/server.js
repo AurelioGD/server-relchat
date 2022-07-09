a815629c6c2f19eb3a6568b01c47c84412171a10
Aurelio Gonzalez <agonzalez@sisu.mx>
2022-07-09 13:51:18 -0500

    Estructura del proyecto

---
+++ src/config/server.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+    SERVER_PORT: process.env.SERVER_PORT
+}
---
+++ src/server/index.js
@@ -0,0 +1,11 @@
+require("module-alias/register")
+
+const app = require("@src/app")
+
+const { SERVER_PORT } = require("@config/server")
+
+const server = app.listen(SERVER_PORT, () => {
+    console.log(`Server started on port ${SERVER_PORT}`)
+})
+
+module.exports = server
